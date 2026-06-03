a27cf696927160c41f9e4de3613fb5ce30f7f3d1
Jessica Kagan <jessica_kagan@outlook.com>
2026-06-03 18:54:22 -0400

    PP-1 Add initial enums for terrain types
    * These currently have no real meaning.

---
+++ src/game/world/TerrainType.ts
@@ -0,0 +1,37 @@
+/**
+ * Enum representing different types of terrain in the game world.
+ * The initial plan is that these should represent biomes.
+ */
+export enum TerrainType {
+    /**
+     * Empty or undefined terrain
+     */
+    EMPTY,
+
+    /** Water terrains */
+
+    OCEAN,
+    ICE_CAP,
+
+    /** Cold land terrains */
+
+    POLAR, 
+    TUNDRA,
+    TAIGA,
+    COLD_DESERT,
+
+    /** Middling temperature land terrains */
+
+    STEPPE,
+    GRASSLAND,
+    TEMPERATE_FOREST,
+    TEMPERATE_SWAMP,
+
+    /** Hot land terrains */
+
+    HOT_DESERT,
+    TROPICAL_GRASSLAND,
+    TROPICAL_FOREST,
+    TROPICAL_SWAMP
+
+}
