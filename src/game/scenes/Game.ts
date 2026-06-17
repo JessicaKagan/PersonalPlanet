7b78f9fe77f5bbbe31207423ecfe3bc42361fa92
Jessica Kagan <jessica_kagan@outlook.com>
2026-06-17 13:57:54 -0400

    PP-3-1 Add initial placeholder control events

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -14,6 +14,16 @@ export class Game extends Scene
     public updateInterval = 1000 / DEFAULT_SIMULATION_TICKS_PER_SECOND; // Update the simulation every 100ms by default.
     public timeSinceLastUpdate = 0; // Keep track of how much time has passed since the last interval.
     
+    /** Generic, top level mouse input handlers which are instantiated when we create the main game scene.
+     * FUTURE: Does Phaser tear down event handlers for its inputs when you leave a scene? If not, how do we handle this?
+     */
+    public onPointerDown?: Phaser.Input.InputPlugin; // Primarily for tracking click and drag events
+    public onPointerUp?: Phaser.Input.InputPlugin;
+    public onPointerMove?: Phaser.Input.InputPlugin;
+    public onPointerWheel?: Phaser.Input.InputPlugin;
+
+    public isPointerHeldDown = false;
+
     constructor ()
     {
         super('Game');
@@ -22,7 +32,7 @@ export class Game extends Scene
     create ()
     {
         this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+        this.camera.setBackgroundColor(0x000000);
 
         // Create a new world with a small test grid (10x10)
         this.world = new World(10, 10);
@@ -33,7 +43,34 @@ export class Game extends Scene
         // Render the initial state of the world
         this.renderWorld();
 
+        this.addControls();
+
         EventBus.emit('current-scene-ready', this);
+    }
+
+    addControls(): void {
+        // TODO: We currently care more about pointer than currentlyOver, but we'll need to make sure we can get references
+        // to the objects users click on.
+        this.input.addListener('pointerdown', (pointer: Phaser.Input.Pointer, currentlyOver: Phaser.GameObjects.GameObject[]) => {
+            this.isPointerHeldDown = true;
+        });
+
+        this.input.addListener('pointerup', (pointer: Phaser.Input.Pointer, currentlyOver: Phaser.GameObjects.GameObject[]) => {
+            this.isPointerHeldDown = false;
+
+            console.log(pointer);
+            console.log(currentlyOver);
+        });
+
+        this.input.addListener('pointermove', (pointer: Phaser.Input.Pointer, currentlyOver: Phaser.GameObjects.GameObject[]) => {
+            if (this.isPointerHeldDown) {
+                // TODO: Moving the mouse while your pointer is held down should result in camera movements, at least for PP-3-1, 
+            }
+        });
+
+        this.input.addListener('wheel', (pointer: Phaser.Input.Pointer, currentlyOver: Phaser.GameObjects.GameObject[]) => {
+            // TODO: Mousewheel events should result in camera zoom, at least within a specific range.
+        });
     }
 
     renderWorld()
