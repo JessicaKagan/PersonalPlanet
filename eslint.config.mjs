fb596a3305e6107843c687ed16d74dfc7764d61a
Jessica Kagan <jessica_kagan@outlook.com>
2026-06-17 15:54:43 -0400

    Add initial Prettier support (WIP)
    * This is more just to make sure it's installed, with a future eye towards improving code consistency. There's more work to be done around ignores, choosing styles, that sort of thing.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 
 import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
 export default defineConfig({
     files: ['**/*.{js,ts}'],
-    extends: [js.configs.recommended, tseslint.configs.recommendedTypeChecked],
+    extends: [js.configs.recommended, tseslint.configs.recommendedTypeChecked, eslintConfigPrettier, eslintPluginPrettierRecommended],
     ignores: ['.svelte-kit/**'],
     languageOptions: {
         parserOptions: {
