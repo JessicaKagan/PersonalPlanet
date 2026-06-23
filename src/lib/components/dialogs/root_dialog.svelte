e07213fa203acaa4583e4ae0605535624849d447
Jessica Kagan <jessica_kagan@outlook.com>
2026-06-23 13:51:40 -0400

    PP-3-2 Add a shared dialog component (WIP)

---
+++ src/lib/components/dialogs/root_dialog.svelte
@@ -0,0 +1,18 @@
+<!-- FUTURE: This component will eventually be the parent of any bespoke dialogs in Personal Planet.
+    This will contain shared styles and event handlers used by, for instance, a settings dialog. -->
+<script lang="ts">
+
+</script>
+
+<dialog>Hello dialog world</dialog>
+
+<style lang="scss">
+    dialog {
+        // Testing styles
+        pointer-events: auto;
+        height: calc(100% * 2/12);
+        width: calc(100% * 2/12);
+        margin: auto;
+        background-color: green;
+    }
+</style>
