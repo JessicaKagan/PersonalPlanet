376d11070758d0f54241540b4f2c2015e2a4608c
Jessica Kagan <jessica_kagan@outlook.com>
2026-06-23 13:51:30 -0400

    PP-3-2 Add a shared menu component and a specific menu to use it
    * Next step is to build out the actual functionality.

---
+++ src/lib/components/menus/world_controls.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import RootMenu from "../root_menu.svelte";
+
+
+</script>
+
+<RootMenu>
+    <div class="container">
+        <li>Hello menu item 1</li>
+        <li>Hello menu item 2</li>
+    </div>
+</RootMenu>
+
+<style lang="scss">
+    .container {
+        height: 100%;
+        width: calc(100% * 1/12);
+
+        li {
+            background-color: blue; // DEBUG
+        }
+    }
+</style>
---
+++ src/lib/components/root_menu.svelte
@@ -0,0 +1,23 @@
+<!-- This component is the parent of any menu bars in Personal Planet, responsible for shared logic and styles.
+    You can include arbitrary content in each menu's template. For example:
+    <RootMenu>
+        <div class="container">
+            <li>Hello menu item 1</li>
+            <li>Hello menu item 2</li>
+        </div>
+    </RootMenu>
+-->
+<script lang="ts">
+
+</script>
+
+<menu>
+    <slot></slot>
+</menu>
+
+<style lang="scss">
+    menu {
+        pointer-events: auto;
+        background-color: green; // DEBUG
+    }
+</style>
