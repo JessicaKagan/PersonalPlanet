a28ae3a7153fc2ff53e10343023fdce3a7ac0ec1
Jessica Kagan <jessica_kagan@outlook.com>
2026-06-25 14:48:50 -0400

    PP-3-2 Add the initial boilerplate for a tile information dialog

---
+++ src/lib/components/dialogs/tile_information.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import RootDialog from "../root_dialog.svelte";
+
+    import type { TPhaserRef } from "../../../PhaserGame.svelte";
+    import type { Game } from "../../../game/scenes/Game";
+
+    interface TileInformationProps {
+        class?: string,
+        phaserRef: TPhaserRef
+    }
+
+    let {
+        class: propsClass = "",
+        phaserRef: phaserRef = { game: null, scene: null }
+    }: TileInformationProps = $props()
+
+</script>
+
+<RootDialog class={propsClass}>
+</RootDialog>
+
+<style lang="scss">
+    :global {
+        background-color: blue; // DEBUG
+    }
+</style>
---
+++ src/lib/components/root_dialog.svelte
@@ -0,0 +1,26 @@
+<!-- FUTURE: This component is the parent of any bespoke dialogs in Personal Planet.
+    You can include arbitrary content in each dialog's template. For example:
+        <RootDialog>
+            <div class="container">
+                <p>Hello dialog world</p>
+            </div>
+        </RootDialog>
+-->
+<script lang="ts">
+
+</script>
+
+<dialog class="dialog-container">
+    <slot></slot>
+</dialog>
+
+<style lang="scss">
+    dialog {
+        // Testing styles
+        pointer-events: auto;
+        height: calc(100% * 2/12);
+        width: calc(100% * 2/12);
+        margin: auto;
+        background-color: green;
+    }
+</style>
