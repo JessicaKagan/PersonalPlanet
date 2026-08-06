d20efab8c968519b53bdca28b352842a637fd9a5
Jessica Kagan <jessica_kagan@outlook.com>
2026-08-06 14:39:20 -0400

    PP-5-2 Update the controls menu to show selected tool and overlay

--- src/lib/components/menus/world_controls.svelte
+++ src/lib/components/menus/world_controls.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import RootMenu from "../root_menu.svelte";
-    import { GraphicsOverlays, WorldControlsTools } from "../../../game/defines/core_defines";
+    import { CustomPhaserEvents, GraphicsOverlays, TileInformationDialogMode, WorldControlsTools } from "../../../game/defines/core_defines";
 
     import type { TPhaserRef } from "../../../PhaserGame.svelte";
     import type { Game } from "../../../game/scenes/Game";
+    import { EventBus } from "../../../game/EventBus";
 
     interface WorldControlProps {
         class?: string,
@@ -15,26 +16,40 @@
         phaserRef: phaserRef = { game: null, scene: null }
     }: WorldControlProps = $props()
 
+    let currentWorldControlTool: WorldControlsTools | undefined = $state();
+    let currentOverlay: GraphicsOverlays | undefined = $state(GraphicsOverlays.None);
+
+    EventBus.on(CustomPhaserEvents.CurrentWorldControlToolSelected, (tool: WorldControlsTools) => {
+        currentWorldControlTool = tool;
+    });
+
     const selectTool = (tool: WorldControlsTools): void => {
         // FUTURE: This "typecast to the relevant scene type" pattern was taken from the Phaser Svelte template.
         // Is this a clean and safe way to get the information we need passed in? If not, we'll want to update it at some point.
         const scene = phaserRef.scene as Game;
 
         if (scene) {
+            // Updating our local reference as well as the game scene reference helps us keep track of state in a Svelte-native way.
             scene.currentWorldControlTool = tool;
+            currentWorldControlTool = tool;
         }
-        
     }
 
     const selectOverlay = (overlay: GraphicsOverlays): void => {
-        // FUTURE: This "typecast to the relevant scene type" pattern was taken from the Phaser Svelte template.
-        // Is this a clean and safe way to get the information we need passed in? If not, we'll want to update it at some point.
         const scene = phaserRef.scene as Game;
 
         if (scene) {
             scene.currentOverlay = overlay;
+            currentOverlay = overlay;
         }
-        
+    }
+
+    const isToolSelected = (tool: WorldControlsTools): boolean=> {
+        return currentWorldControlTool === tool;
+    }
+
+    const isOverlaySelected = (overlay: GraphicsOverlays): boolean=> {
+        return currentOverlay === overlay;
     }
 
 </script>
@@ -42,12 +57,36 @@
 <RootMenu class={propsClass}>
     <!-- FUTURE: We should consider sourcing different icons here, since emojis vary from platform to platform.
         These could either be images or SVGs. Potentially even a combination of the two. Material Symbols works well for the latter. -->
-    <button class="controls" title="Query" aria-label="query" onclick={() => selectTool(WorldControlsTools.Query)}>🔍</button>
-    <button class="controls" title="Draw" aria-label="draw" onclick={() => selectTool(WorldControlsTools.Draw)}>🖌️</button>
+    <button
+        class="controls {isToolSelected(WorldControlsTools.Query) ? 'selected' : ''}"
+        title="Query"
+        aria-label="query"
+        onclick={() => selectTool(WorldControlsTools.Query)}>
+        🔍
+    </button>
+    <button
+        class="controls {isToolSelected(WorldControlsTools.Draw) ? 'selected' : ''}"
+        title="Draw"
+        aria-label="draw"
+        onclick={() => selectTool(WorldControlsTools.Draw)}>
+        🖌️
+    </button>
 
     <!-- Overlay controls -->
-    <button class="controls" title="Disable All Overlays" aria-label="disable all overlays" onclick={() => selectOverlay(GraphicsOverlays.None)}>🪟</button>
-    <button class="controls" title="Show Life" aria-label="show life" onclick={() => selectOverlay(GraphicsOverlays.Lifeforms)}>🧬</button>
+    <button
+        class="controls {isOverlaySelected(GraphicsOverlays.None) ? 'selected' : ''}"
+        title="Disable All Overlays"
+        aria-label="disable all overlays"
+        onclick={() => selectOverlay(GraphicsOverlays.None)}>
+        🪟
+    </button>
+    <button
+        class="controls {isOverlaySelected(GraphicsOverlays.Lifeforms) ? 'selected' : ''}"
+        title="Show Life"
+        aria-label="show life"
+        onclick={() => selectOverlay(GraphicsOverlays.Lifeforms)}>
+        🧬
+    </button>
 </RootMenu>
 
 <style lang="scss">
@@ -88,6 +127,10 @@
 
             &:active {
                 filter: drop-shadow(0 0 4px black);
+            }
+
+            &.selected {
+                background-color: #ffffffc0;
             }
         }
     }
