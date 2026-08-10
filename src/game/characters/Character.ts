3a4a8aa49ed655ea04b704aedc3b64bc899753a2
Jessica Kagan <jessica_kagan@outlook.com>
2026-08-10 15:20:40 -0400

    PP-6 Add the initial scaffolding for a Character class

---
+++ src/game/characters/Character.ts
@@ -0,0 +1,48 @@
+/**
+ * Classes and methods for representing autonomous agents in the player's World.
+ */
+
+import { v4 as uuidv4 } from 'uuid';
+import type { Lifeform } from '../world/Lifeforms';
+
+/** The properties in this interface are used to populate initial information for Characters in the player's World.
+ * Using a typed object reduces the odds of developer error.
+ */
+interface CharacterConstructorArgs {
+    name: string;
+    species: Lifeform;
+    spriteKey: string;
+    timeCreated: number;
+    x?: number;
+    y?: number;
+    isProtected: boolean;
+}
+
+export class Character {
+    private readonly id: string;
+    public readonly timeCreated: number; // TODO: Should this be a simulation tick?
+
+    public name: string;
+    public species: Lifeform;
+    public spriteKey: string;
+    public isProtected: boolean;
+
+    public x?: number;
+    public y?: number;
+
+    // TODO: Figure out how to represent allowed behaviors and their time costs (i.e, how often can the character attempt to move?).
+    // We should most likely use setters here to allow for error trapping. This is handy if potential behaviors are mutually exclusive.
+    private allowedBehaviors = [];
+
+    constructor(args: CharacterConstructorArgs) {
+        this.id = uuidv4();
+        this.timeCreated = args.timeCreated; // DEBUG
+        this.name = args.name;
+        this.species = args.species;
+        this.spriteKey = args.spriteKey;
+        this.isProtected =  args.isProtected;
+
+        this.x = args.x ?? undefined;
+        this.y = args.y ?? undefined;
+    }
+}
